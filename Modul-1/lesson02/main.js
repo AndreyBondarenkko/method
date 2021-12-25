c5a8c97ff324cb12e12eaaa420c4a25e2c1dd5b9
Andrey Bondarenko <andrey.bondarenko.devmrks@gmail.com>
2021-12-25 18:15:01 +0200

    lesson02

--- Modul-1/lesson02/main.js
+++ Modul-1/lesson02/main.js
@@ -1,9 +1,9 @@
-{ 
-    const product = 'MacBook';
-    const productColor = 'Silver';
-    const productCount = 3;
-    const productCategory = 'Device';
-    const productCost = 40000;
-    console.log(product);
-    console.log(productCost * productCount); 
-}
+{
+    const item = 'MacBook';
+    const count = 15;
+    const category = 'Device';
+    const price = 40000;
+  
+    console.log(item);
+    console.log(`общая сумма товара: ${count * price}`);
+  }
